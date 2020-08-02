3114b59dca863eb96456463e3798d570c56bff9a
Keith Wong <42085249+keithwongg@users.noreply.github.com>
2020-08-02 13:56:54 +0800

    changed max digits in models for total amount spent

--- visualizer/models.py
+++ visualizer/models.py
@@ -14,7 +14,7 @@ class Referral(models.Model):
     Order_Number = models.CharField(max_length=200)
     Completed_At = models.DateField(blank=True, null= True)
     State = models.CharField(max_length=200)
-    Total_Amount_Spent = models.DecimalField(max_digits=10000000,decimal_places=2)
+    Total_Amount_Spent = models.DecimalField(max_digits=999,decimal_places=2)
 
     # def __str__(self):
     #     return self.Advocate_First_Name
